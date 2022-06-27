c95443ffcdc09c8a9c7ea15c57501c6f6d22d6fb
maguibing <kuse012@163.com>
2022-06-27 14:06:39 +0800

    webpack基础练习

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,18 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 const resolvePath = (_path) => {
   return path.resolve(__dirname, _path)
 }
+const resolveHtmlPath = (_path) => path.resolve(__dirname, _path)
 
 module.exports = {
   // 模式
   mode: 'development',
-  // 入口文件 入口可以有多个
+  // 入口 入口可以有多个
   entry: './src/index.js',
-  // 出口 出口只能有一个 并且必须保证是绝对路径
+  // 出口 只能有一个 并且必须保证是绝对路径
   output: {
     filename: 'main.js',
     path: resolvePath('dist'),
@@ -21,32 +24,26 @@ module.exports = {
     // 模块规则
     rules: [
       {
-        // css 文件
         test: /\.css$/,
-        // 处理css 使用的 loader(加载器)
-        use: ['style-loader', 'css-loader'],
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
       },
       {
-        // less 文件
         test: /\.less$/,
-        // 处理css 使用的 loader(加载器)
-        use: ['style-loader', 'css-loader', 'less-loader'],
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'less-loader'],
       },
       {
-        // scss 文件
         test: /\.s[ac]ss$/,
-        // 处理css 使用的 loader(加载器)
-        use: ['style-loader', 'css-loader', 'sass-loader'],
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
       },
       {
-        // 图片 文件
         test: /\.(jpe?g|png|gif|webp|svg)$/,
         type: 'asset',
         generator: {
           filename: 'assets/img/[hash:10][ext]',
         },
       },
-      // 使用babel 处理js 以及新语法
+
+      // 使用babel 处理es6以及新语法
       {
         test: /\.js$/,
         exclude: /node_modules/,
@@ -58,5 +55,24 @@ module.exports = {
         },
       },
     ],
+  },
+
+  // 插件
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: resolveHtmlPath('./src/index.html'),
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'css/[name].css',
+    }),
+  ],
+
+  devServer: {
+    // 加入下面这段告知 webpack-dev-server，将 './' 目录下的文件 serve 到 localhost:8080 下(寄存到服务器下)  static: "./"
+    static: './',
+    host: 'localhost',
+    port: 8080,
+    open: true,
+    hot: true,
   },
 }
